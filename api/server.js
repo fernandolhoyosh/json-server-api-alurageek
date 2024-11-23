f02ce984d6b6f111d2c00df0038f5531bcd36036
Fernando Hoyos <fernandolhoyosh@gmail.com>
2024-11-22 21:18:39 -0500

    configurando CORS

--- api/server.js
+++ api/server.js
@@ -1,8 +1,14 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+//CORS
+server.use(cors({
+    origin: '*'
+}));
 
 server.use(middlewares)
 // Add this before server.use(router)
